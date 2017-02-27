5ffb1ee6bf57d486eabfc7da356983ead0252dfe
Timo Mechsner <timo.mechsner@mailbox.org>
2017-02-27 17:45:27 +0100

    Spring Boot Init Commit

---
+++ src/main/java/org/polidise/PolidiseApplication.java
@@ -0,0 +1,13 @@
+package org.polidise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PolidiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolidiseApplication.class, args);
+	}
+}
